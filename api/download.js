48abd8694aec8a3a295a22f96232159efc06ac1a
The-Medellin-Adventure <dcaame@gmail.com>
2025-10-09 19:59:59 -0500

    Update download.js

--- api/download.js
+++ api/download.js
@@ -26,7 +26,6 @@ export default async function handler(req, res) {
     const url = `${baseUrl}/${encodeURIComponent(file)}?Authorization=${token}`;
 
     console.log("✅ URL generada:", url);
-
     return res.status(200).json({ downloadUrl: url });
   } catch (err) {
     console.error("❌ Error en descarga Backblaze:", err);
